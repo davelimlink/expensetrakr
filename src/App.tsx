7c0d48214ef985abc12d348e0ce3b0f4628c4a93
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2024-07-19 17:29:05 +0900

    React Hook From and Zod for handling errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseFilter from "./components/ExpenseFilter";
 import ExpenseForm from "./components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./components/categories.ts";
 
 const App = () => {
   const [selectedCategory, setSelectedCategory] = useState("");
